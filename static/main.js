f2b85605f7c318b89c9592e68bfb90c2afd46718
Asad Jalib <62315926+Asadjalib@users.noreply.github.com>
2020-05-18 14:48:08 -0400

    added clock
    add live timer to nav bar

--- static/main.js
+++ static/main.js
@@ -1,3 +1,24 @@
 function update() {
     document.querySelector('#total').innerHTML=total;
 }
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var suf = "AM"
+    if (h >= 12) {
+    h = h - 12;
+    suf = "PM";
+  }
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s + " "+ suf;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
